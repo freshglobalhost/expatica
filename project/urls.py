75f4eeb5cea01f448cbc22d72656b52d8eac66ed
Ezeche <freshdecoder@gmail.com>
2026-05-21 17:19:48 +0100

    first-commt

--- project/urls.py
+++ project/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("site/admin/", admin.site.urls),
     path("api/v1/", include("project.api_urls")),
 ]
 

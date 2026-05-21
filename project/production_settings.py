0f32e622d822416656b7951824b356f33dba9f1a
Ezeche <freshdecoder@gmail.com>
2026-05-22 00:08:04 +0100

    first-commt

--- project/production_settings.py
+++ project/production_settings.py
@@ -1,77 +1,30 @@
-
 import os
 import dj_database_url
-
-from project.base_settings import *  # noqa
-from project.settings.packages.celery_settings import *  # noqa
-from project.settings.local.email_settings import *  # noqa
+from project.base_settings import *  # noqa: F403
+from project.settings.packages.celery_settings import *  # noqa: F403
+from project.settings.local.email_settings import *  # noqa: F403
 from project.utils.settings import get_env_variable
-
-
 SECRET_KEY = get_env_variable("SECRET_KEY")
-
-DEBUG = get_env_variable("DEBUG", "0") == "1"
-
-
+DEBUG = int(get_env_variable("DEBUG", "0"))
 INSTALLED_APPS.append("storages")
+DATABASES["default"] = dj_database_url.parse(get_env_variable("DATABASE_URL"), conn_max_age=600)
 
-
-DATABASES["default"] = dj_database_url.parse(
-    get_env_variable("DATABASE_URL"),
-    conn_max_age=600,
-)
-
-
-ALLOWED_HOSTS = [
-    host.strip()
-    for host in get_env_variable(
-        "DJANGO_ALLOWED_HOSTS",
-        "api.pennycreditonline.com,pennycreditonline.com,www.pennycreditonline.com",
-    ).split(",")
-    if host.strip()
-]
-
+ALLOWED_HOSTS = ["api.pennycreditonline.com", "pennycreditonline.com","www.pennycreditonline.com"]
 
 CORS_ALLOWED_ORIGINS = [
-    origin.strip()
-    for origin in get_env_variable(
-        "CORS_ALLOWED_ORIGINS",
-        "https://pennycreditonline.com,https://www.pennycreditonline.com",
-    ).split(",")
-    if origin.strip()
+    "https://pennycreditonline.com",
+    "https://api.pennycreditonline.com",
+    "https://www.pennycreditonline.com"
 ]
-
 
 CORS_ALLOW_CREDENTIALS = True
 
 
-CORS_ALLOW_HEADERS = [
-    "accept",
-    "accept-encoding",
-    "authorization",
-    "content-type",
-    "dnt",
-    "origin",
-    "user-agent",
-    "x-csrftoken",
-    "x-requested-with",
-]
-
-
 CSRF_TRUSTED_ORIGINS = CORS_ALLOWED_ORIGINS
 
-
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-
-
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
-
-
-SESSION_COOKIE_SAMESITE = "None"
-CSRF_COOKIE_SAMESITE = "None"
-
-
 
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': 'dl9r87apa',
